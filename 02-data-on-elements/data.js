509ec2353ab3b5e69af4ef7113b1486e7bf51427
Ryan Goggin <ryangoggin1@gmail.com>
2022-12-21 13:37:13 -0500

    finished 02 practice

--- 02-data-on-elements/data.js
+++ 02-data-on-elements/data.js
@@ -1,1 +1,24 @@
 // Your code here
+window.addEventListener("DOMContentLoaded", () => {
+    //add item to list
+    const addButton = document.querySelector("#add");
+    addButton.type = "button";
+    const shoppingList = document.querySelector("#shopping-list");
+
+    const add = () => {
+        const input = document.querySelector("#name");
+        const value = input.value;
+
+        const select = document.querySelector("#type");
+        const type = select.value;
+
+        const newLi = document.createElement("li");
+        newLi.innerText = value;
+        newLi.setAttribute("data-type", type);
+
+        shoppingList.append(newLi);
+        input.value = "";
+    }
+
+    addButton.addEventListener("click", add);
+});
